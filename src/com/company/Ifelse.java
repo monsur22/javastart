5e8171cb9011225345f9e6a7c2ba0350f7b107b7
monsur22 <monsurahmedshafiq@gmail.com>
2021-10-15 22:33:33 +0600

    First Commit

---
+++ src/com/company/Ifelse.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Ifelse {
+    public static void main(String[] args){
+        int age = 10;
+        if(age>=20){
+            System.out.println("Age is getter than 20");
+        }
+        else {
+            System.out.println("Small or equal from 20");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a+b;
+        String d = "Total number";
+        System.out.println(d);
+        System.out.println(c);
+
+    }
+}
